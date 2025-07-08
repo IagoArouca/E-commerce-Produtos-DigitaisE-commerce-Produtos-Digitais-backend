67ad68868dd39e149d90fff54023ec389053c954
Iago <iagoaroucaferreira@hotmail.com>
2025-07-08 13:14:50 -0300

    Criei as imagens de forma static para rodar na pagina

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const connectDB = require('./config/db'); // Importa a função de conexão com o DB
 const productRoutes = require('./routes/productRoutes'); // Importa as rotas de produto
 const authRoutes = require('./routes/authRoutes'); 
+const path = require('path');
 
 require('dotenv').config(); // Carrega as variáveis de ambiente
 
@@ -16,6 +17,8 @@ connectDB();
 // Middlewares
 app.use(cors()); // Habilita o CORS para todas as requisições
 app.use(express.json()); // Permite que o Express parseie requisições com JSON no corpo
+
+app.use('/images', express.static(path.join(__dirname, 'public', 'images')));
 
 // Rota de Teste Simples
 app.get('/', (req, res) => {
